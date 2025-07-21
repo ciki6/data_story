49210977f5dda0b20fdb83308a3e17f2fea46fff
LuXiaoFu <18694942447@163.com>
2025-07-21 17:32:39 +0800

    update buttinasd777

--- src/components/DataBinding.tsx
+++ src/components/DataBinding.tsx
@@ -1,11 +1,13 @@
-import { Button, Space, Dropdown, message } from "antd";
-import type { MenuProps } from "antd";
+import { Button, Space, Dropdown, message, Upload } from "antd";
+import type { MenuProps, UploadProps } from "antd";
+import { read, utils } from "xlsx";
 import { UploadOutlined, DownOutlined } from "@ant-design/icons";
 import { useDataStore } from "../store/dataStore";
 import type { FC } from "react";
 
 const DataBinding: FC = () => {
   const tableData = useDataStore((state) => state.tableData);
+  const setTableData = useDataStore((state) => state.setTableData);
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     message.info("Click on left button.");
     console.log("click left button", e);
@@ -29,6 +31,41 @@ const DataBinding: FC = () => {
       key: "3",
     },
   ];
+  const uploadProps: UploadProps = {
+    accept: ".xlsx,.xls",
+    showUploadList: false,
+    beforeUpload: (file) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const data = new Uint8Array(e.target?.result as ArrayBuffer);
+          const wb = read(data);
+          const ws = wb.Sheets[wb.SheetNames[0]];
+          const jsonData = utils.sheet_to_json(ws, { header: 1 });
+
+          // 转换为二维数组格式
+          const twoDArray = jsonData as any[][];
+          setTableData(twoDArray);
+        } catch (error) {
+          message.error("文件解析失败:");
+        }
+      };
+      reader.readAsArrayBuffer(file);
+
+      // 阻止默认上传行为
+      return false;
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`);
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+  };
   const menuProps = {
     items,
     onClick: handleMenuClick,
@@ -47,13 +84,15 @@ const DataBinding: FC = () => {
         style={{ width: "100%" }}
       >
         <div>
-          <Button
-            icon={<UploadOutlined />}
-            onClick={() => handleButtonClick}
-            style={{ borderRadius: "6px 0 0 6px" }}
-          >
-            上传Excel
-          </Button>
+          <Upload {...uploadProps}>
+            <Button
+              icon={<UploadOutlined />}
+              onClick={() => handleButtonClick}
+              style={{ borderRadius: "6px 0 0 6px" }}
+            >
+              上传Excel
+            </Button>
+          </Upload>
           <Dropdown
             menu={menuProps}
             placement="bottom"
